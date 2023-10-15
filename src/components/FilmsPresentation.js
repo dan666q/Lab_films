0c3fe4f053116a3734448750d964bfb032815674
Dan <brunotran1602@gmail.com>
2023-10-15 17:38:53 +0700

    update json file

--- src/components/FilmsPresentation.js
+++ src/components/FilmsPresentation.js
@@ -3,7 +3,7 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import { Button, CardActionArea, CardActions, Container, Box } from '@mui/material';
+import { Button, CardActionArea, CardActions, Container, Box, Tooltip } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { Films } from '../shared/ListOfFilm';
 export default function FilmsPresentation() {
@@ -14,21 +14,27 @@ export default function FilmsPresentation() {
                 {Films.map((film) => (
                     <div className='col-md-3'>
                         <Card sx={{ maxWidth: 250, }} className='mt-5'>
-                                <CardMedia
-                                    component="img"
-                                    height="400"
-                                    src={film.Image}
-                                    alt="player"
-                                />
-                                <CardContent >
-                                    <Typography gutterBottom variant="h5" component="div" className='ellipsis'>
-                                        {film.Title}
-                                    </Typography>
-                                    <p>{film.Year}</p>
-                                    {/* <Typography variant="body2" color="text.secondary" >
+                            <CardMedia
+                                component="img"
+                                height="400"
+                                src={film.Image}
+                                alt="player"
+                            />
+                            <CardContent >
+                                <Tooltip title={film.Title} arrow>
+                                <Typography gutterBottom variant="h6" component="div" className='ellipsis'>
+                                    {film.Title}
+                                </Typography>
+                                </Tooltip>
+
+                           
+                                <Typography gutterBottom variant="h7" component="div" className='ellipsis'>
+                                    {film.Year}
+                                </Typography>
+                                {/* <Typography variant="body2" color="text.secondary" >
                             {player.info}
                             </Typography> */}
-                                </CardContent>
+                            </CardContent>
                             <CardActions>
                                 <Link to={`Detail/${film.id}`}>
                                     <Button size="small" color="primary">
