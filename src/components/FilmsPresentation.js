6436edb0f606d73092166dd4a9885c55bb4004e7
Dan <brunotran1602@gmail.com>
2023-10-23 00:34:10 +0700

    apiV1

--- src/components/FilmsPresentation.js
+++ src/components/FilmsPresentation.js
@@ -1,17 +1,31 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { Button, CardActionArea, CardActions, Container, Box, Tooltip, FormControl, InputLabel, Select, MenuItem } from '@mui/material';
 import { Link } from 'react-router-dom';
-import { Films } from '../shared/ListOfFilm';
+// import { Films } from '../shared/ListOfFilm';
 import Carousel from './Carousel';
 import { useState } from 'react';
+import axios from 'axios';
 
 export default function FilmsPresentation() {
+    const [films, setFilms] = useState([]);
+    useEffect(() => {
+        getData();
+    }, [])
+
+    function getData() {
+        const res = axios.get(`https://64abe6529edb4181202ec3ba.mockapi.io/Api/Films`)
+            .then(function (response) {
+                setFilms(response.data)
+            }).catch(function (error) {
+                console.log(error);
+            });
+    }
     const [sortBy, setSortBy] = useState(null);
-    const sortedFilms = [...Films].sort((a, b) => {
+    const sortedFilms = [...films].sort((a, b) => {
         if (sortBy === "name") {
             return a.Title.localeCompare(b.Title);
         } else if (sortBy === "year") {
@@ -20,10 +34,10 @@ export default function FilmsPresentation() {
             return 0; // No sorting
         }
     });
-    
+
     return (
         <Container>
-            <Carousel/>
+            <Carousel />
             <p className='fs-2 mt-5'>List Of Films</p>
             <FormControl variant="standard" sx={{ minWidth: 120 }}>
                 <InputLabel>Sort By</InputLabel>
@@ -38,29 +52,29 @@ export default function FilmsPresentation() {
             <div className='row'>
                 {sortedFilms.map((film) => (
                     <div className='col-lg-3 col-md-4 col-sm-6'>
-                        <Container sx={{display: 'flex', justifyContent: 'center'}}>
-                        <Card sx={{ maxWidth: 250, }} className='mt-5'>
-                            <CardActionArea >
-                                <Link to={`/${film.id}`} style={{ textDecoration: 'none', color: "inherit" }}>
-                                    <CardMedia
-                                        component="img"
-                                        height="auto"
-                                        src={film.Image}
-                                        alt="film"
-                                    />
-                                    <CardContent >
-                                        <Tooltip title={film.Title} arrow>
-                                            <Typography gutterBottom variant="h6" component="div" className='ellipsis'>
-                                                {film.Title}
+                        <Container sx={{ display: 'flex', justifyContent: 'center' }}>
+                            <Card sx={{ maxWidth: 250, }} className='mt-5'>
+                                <CardActionArea >
+                                    <Link to={`/${film.id}`} style={{ textDecoration: 'none', color: "inherit" }}>
+                                        <CardMedia
+                                            component="img"
+                                            height="auto"
+                                            src={film.Image}
+                                            alt="film"
+                                        />
+                                        <CardContent >
+                                            <Tooltip title={film.Title} arrow>
+                                                <Typography gutterBottom variant="h6" component="div" className='ellipsis'>
+                                                    {film.Title}
+                                                </Typography>
+                                            </Tooltip>
+                                            <Typography gutterBottom variant="h7" component="div" className='ellipsis'>
+                                                {film.Year}
                                             </Typography>
-                                        </Tooltip>
-                                        <Typography gutterBottom variant="h7" component="div" className='ellipsis'>
-                                            {film.Year}
-                                        </Typography>
-                                    </CardContent>
-                                </Link>
-                            </CardActionArea>
-                        </Card>
+                                        </CardContent>
+                                    </Link>
+                                </CardActionArea>
+                            </Card>
                         </Container>
                     </div>
                 ))}
